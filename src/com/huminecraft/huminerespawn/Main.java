6926b19034d7a3c574a1f1b2089fe23fba77983e
Simon Scatton <simon.scatton@outlook.fr>
2019-03-12 23:08:18 +0100

    Initial Commit

---
+++ src/com/huminecraft/huminerespawn/Main.java
@@ -0,0 +1,15 @@
+package com.huminecraft.huminerespawn;
+
+public class Main {
+    
+    final static String VERSION = "1.0";
+    
+    public void onEnable() {
+	System.out.println("Humine Respawn " + VERSION + " by SDA loaded.");
+    }
+    
+    public void onDisable() {
+	
+    }
+
+}
